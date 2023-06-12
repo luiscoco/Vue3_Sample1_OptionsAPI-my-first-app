e43f613564651c9ffbe5c47bbd52c59c62333841
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-06-12 15:43:57 +0200

    first-commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>{{ message }}</p>
+    <button @click="updateMessage">Update Message</button>
+  </div>
+</template>
+
+<!-- Options API -->
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello, Vue!",
+    };
+  },
+  methods: {
+    updateMessage() {
+      this.message = "Hello, Vue updated!";
+    },
+  },
+};
+</script>
